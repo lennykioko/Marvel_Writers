15d7948becfc5d8d07a9ba2ed44544af5fd5017c
lennykioko <lennykmutua@gmail.com>
2017-12-09 21:21:10 +0300

    [FIX] Modify settings.py to allow staticfiles collection

--- united/united/settings.py
+++ united/united/settings.py
@@ -35,6 +35,20 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
+# configuration for heroku static files
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -58,7 +72,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+
+# enables gzip functionality
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'united.urls'
 
